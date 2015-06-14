033d0b9744a2a34833e2650b9579416ce389f6ea
Philipp Alferov <philipp.alferov@gmail.com>
2015-06-14 01:46:05 -0400

    Add tests

---
+++ test/test-creation.js
@@ -0,0 +1,41 @@
+/*global describe, before, it*/
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+
+describe('angular with gulp and browserify generator', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({ skipInstall: true })
+      .on('end', done);
+  });
+
+  it('it should create files', function () {
+
+    assert.file([
+      'package.json',
+      'gulpfile.js',
+      '.jshintrc',
+      '.gitignore'
+    ]);
+
+  });
+
+  it('it should create nested files and folders', function () {
+
+    assert.file([
+      'app/index.html',
+      'app/js/main.js',
+      'app/js/controllers/_index.js',
+      'gulp/index.js',
+      'gulp/tasks/browserify.js',
+      'test/karma.conf.js',
+      'test/e2e/example_spec.js',
+      'test/unit/controllers/example_spec.js'
+    ]);
+
+  });
+
+});
