1ce2a15d0bef41fc45c143b6210173297cde40bb
Philipp Alferov <philipp.alferov@gmail.com>
2015-06-18 16:24:13 -0400

    Fix .gitignore not found error

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,7 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var util = require('util');
+var fs = require('fs');
 
 var Generator = module.exports = function (args, options) {
   yeoman.generators.Base.apply(this, arguments);
@@ -10,18 +11,19 @@ var Generator = module.exports = function (args, options) {
 
 util.inherits(Generator, yeoman.generators.Base);
 
-Generator.prototype.welcome = function () {
+Generator.prototype.welcome = function() {
   if (!this.options['skip-welcome-message']) {
     this.log(yosay(
       [
         chalk.red('Welcome!'),
-        chalk.yellow('You\'re using the fantastic generator for scaffolding an application with AngularJS, SASS, Gulp, and Browserify!')
+        chalk.yellow(['You\'re using the fantastic generator for scaffolding an',
+        'application with AngularJS, SASS, Gulp, and Browserify!'].join('\s'))
       ].join('\n')
     ));
   }
 };
 
-Generator.prototype.askFor = function () {
+Generator.prototype.askFor = function() {
   var cb = this.async(),
     prompts = [{
       type: 'confirm',
@@ -36,23 +38,52 @@ Generator.prototype.askFor = function () {
   }.bind(this));
 };
 
-Generator.prototype.app = function () {
+Generator.prototype.app = function() {
   this.directory('app', 'app');
-  this.copy('.gitignore', '.gitignore');
   this.copy('.jshintrc', '.jshintrc');
-
 };
 
-Generator.prototype.test = function () {
+Generator.prototype.gitignore = function() {
+  var templateRoot = this.templatePath('');
+
+  if (fs.existsSync(templateRoot + '/.gitignore')) {
+    this.copy('.gitignore', '.gitignore');
+    return ;
+  }
+
+  //If generator templates are served by npm (npm serves .gitignore as
+  //.npmignore), generator treats .npmignore as .gitignore
+  if (fs.existsSync(templateRoot + '/.npmignore')) {
+    this.copy('.npmignore', '.gitignore');
+    return ;
+  }
+
+  var output = this.destinationPath('.gitignore');
+  var content = ['.DS_Store', '# Logs', 'logs', '*.log', '# Runtime data', 'pids',
+    'lib-cov', '# Coverage directory used by tools like istanbul',
+    'coverage', '# Compiled binary addons (http://nodejs.org/api/addons.html)',
+    'build/Release', '# Dependency directories', 'node_modules',
+    'bower_components', '# Build files', 'build', 'templates.js', '#webstorm',
+    '.idea'
+  ].join('\n');
+
+  fs.writeFile(output, content, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+  });
+};
+
+Generator.prototype.test = function() {
   this.directory('test', 'test');
 };
 
-Generator.prototype.gulp = function () {
+Generator.prototype.gulp = function() {
   this.directory('gulp', 'gulp');
   this.copy('gulpfile.js', 'gulpfile.js');
 };
 
-Generator.prototype.npm = function () {
+Generator.prototype.npm = function() {
   this.copy('package.json', 'package.json');
 
   if (this.installNpmDep) {
