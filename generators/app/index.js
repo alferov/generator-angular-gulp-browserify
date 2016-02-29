b261cb36dd6ff933e04c2fe4f4ac732736afa40e
Philipp Alferov <philipp.alferov@gmail.com>
2016-02-28 23:11:16 -0500

    Refactoring

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,25 +3,12 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var util = require('util');
-var fs = require('fs');
-var path = require('path');
 var glob = require('glob');
-var objectAssign = require('object-assign');
+var _ = require('lodash');
+var extend = _.assign;
 
 // Helpers
-var isArray = function(obj) {
-  return Array.isArray(obj);
-};
-
-var isObject = function(obj) {
-  return obj !== null && typeof obj === 'object';
-};
-
-var isFunction = function(obj) {
-  return typeof obj === 'function';
-};
-
-var betterTypeErrors = function (expected, obj) {
+var betterTypeErrors = function(expected, obj) {
   var type = typeof obj;
   throw new TypeError('Expected ' + expected + ' but got ' +  type);
 };
@@ -30,11 +17,11 @@ var betterTypeErrors = function (expected, obj) {
 var copyExcept = function(obj, except) {
   var result = {};
 
-  if (!isArray(except)) {
+  if (!_.isArray(except)) {
     betterTypeErrors('an array', obj);
   }
 
-  if (!isObject(obj)) {
+  if (!_.isObject(obj)) {
     betterTypeErrors('an object', obj);
   }
 
@@ -48,22 +35,6 @@ var copyExcept = function(obj, except) {
   return result;
 };
 
-var difference = function(compareWith, compareTo) {
-  if (!isArray(compareWith)) {
-    betterTypeErrors('an array', compareWith);
-  }
-  if (!isArray(compareTo)) {
-    betterTypeErrors('an array', compareTo);
-  }
-
-  return compareWith.reduce(function(prev, val){
-    if (compareTo.indexOf(val) < 0) {
-      prev.push(val);
-    }
-    return prev;
-  }, []);
-}
-
 // Generator initialization
 var Generator = module.exports = function() {
   yeoman.generators.Base.apply(this, arguments);
@@ -72,14 +43,17 @@ var Generator = module.exports = function() {
 util.inherits(Generator, yeoman.generators.Base);
 
 Generator.prototype.welcome = function() {
-  if (!this.options['skip-welcome-message']) {
-    this.log(yosay([
-        chalk.red('Welcome!'),
-        chalk.yellow(['You\'re using the fantastic generator for scaffolding an',
-        'application with AngularJS, SASS, Gulp, and Browserify!'].join('\s'))
-      ].join('\n')
-    ));
+  if (this.options['skip-welcome-message']) {
+    return ;
   }
+
+  var title = 'Welcome!';
+  var text = [
+    'You\'re using the fantastic generator for scaffolding an',
+    'application with AngularJS, SASS, Gulp, and Browserify!'
+  ].join('\s');
+
+  this.log(yosay([chalk.red(title), chalk.yellow(text)].join('\n')));
 };
 
 Generator.prototype.askForGeneratorName = function() {
@@ -97,81 +71,41 @@ Generator.prototype.askForGeneratorName = function() {
 };
 
 Generator.prototype.copyAll = function() {
-  var templateRoot = this.sourceRoot();
-  var destinationRoot = this.destinationRoot();
-  var files = glob.sync('**', { dot: true, nodir: true, cwd: templateRoot });
-  var cp = this.copy;
+  var files = glob.sync('**', { dot: true, nodir: true, cwd: this.sourceRoot() });
   var exclude = ['package.json', '.gitignore', '.npmignore'];
-  var diff = difference(files, exclude);
+  var diff = _.difference(files, exclude);
 
   diff.forEach(function(value) {
-    var dest = path.join(destinationRoot, value);
-    cp.call(this, path.join(templateRoot, value), dest);
+    this.copy(this.templatePath(value), this.destinationPath(value));
   }.bind(this));
 };
 
 Generator.prototype.gitignore = function() {
-  var templateRoot = this.sourceRoot();
-  var done = this.async();
+  if (this.fs.exists(this.templatePath('.gitignore'))) {
+    this.copy('.gitignore', '.gitignore');
+    return ;
+  }
 
-  var onGitignoreOpen = function(exists) {
-    if (exists) {
-      this.copy('.gitignore', '.gitignore');
-      done();
-    }
-  };
-
-  var onNpmignoreOpen = function(exists) {
-    if (exists) {
-      this.copy('.npmignore', '.gitignore');
-      done();
-    }
-  };
-
-  fs.exists(templateRoot + '/.gitignore', onGitignoreOpen.bind(this));
-  fs.exists(templateRoot + '/.npmignore', onNpmignoreOpen.bind(this));
+  if (this.fs.exists(this.templatePath('.npmignore'))) {
+    this.copy('.npmignore', '.gitignore');
+  }
 };
 
 Generator.prototype.packageJSON = function() {
   var templateRoot = this.sourceRoot();
-  var destinationRoot = this.destinationRoot();
-  var packageJson = templateRoot + '/package.json';
-  var done = this.async();
-
-  var onPackageJsonSync = function(err, content) {
-    var exclude = [
-      'author',
-      'repository',
-      'private',
-      'engines'
-    ];
-    var newValues = {
-      name: this.appname,
-      version: '1.0.0'
-    }
-
-    if (err) {
-      this.log.error('Could not open package.json for reading.', err);
-      done();
-      return ;
-    }
-
-    try {
-      var pkg = JSON.parse(content);
-    } catch(err) {
-      this.log.error('Could not parse package.json', err);
-    } finally {
-      done();
-    }
-
-    var copy = objectAssign(copyExcept(pkg, exclude), newValues);
-    var json = JSON.stringify(copy, null, 2);
-    var filename = destinationRoot + '/package.json';
-
-    fs.writeFile(filename, json, done);
+  var pkg = this.fs.readJSON(templateRoot + '/package.json');
+  var exclude = [
+    'author',
+    'repository',
+    'private',
+    'engines'
+  ];
+  var newValues = {
+    name: this.appname,
+    version: '1.0.0'
   };
-
-  fs.readFile(packageJson, { encoding: 'utf-8' }, onPackageJsonSync.bind(this));
+  var copy = extend(copyExcept(pkg, exclude), newValues);
+  this.fs.writeJSON(this.destinationPath('package.json'), copy);
 };
 
 Generator.prototype.npm = function() {
